9a07c170a6f701bc61fcdda75be0fdb4e1722dcf
Nikolay Panov <pannikolaserg@gmail.com>
2026-06-01 01:17:12 +0900

    add crud for courses and lessons

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+
+# Курс - ViewSet (всё в одном классе)
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+# Урок - Generic классы (только 2 класса)
+class LessonListCreateAPIView(ListCreateAPIView):
+    """GET список уроков, POST создать урок"""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    """GET один урок, PUT/PATCH обновить, DELETE удалить"""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
