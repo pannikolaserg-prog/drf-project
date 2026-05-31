27ac3a0b658e775ba6d3b260d39f9619036cde4b
Nikolay Panov <pannikolaserg@gmail.com>
2026-06-01 02:04:57 +0900

    add final correctings

--- materials/urls.py
+++ materials/urls.py
@@ -1,12 +1,18 @@
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from django.urls import path, include
-from .views import CourseViewSet, LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView
+
+from .views import (CourseViewSet, LessonListCreateAPIView,
+                    LessonRetrieveUpdateDestroyAPIView)
 
 router = SimpleRouter()
-router.register('courses', CourseViewSet)
+router.register("courses", CourseViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('lessons/', LessonListCreateAPIView.as_view(), name='lesson_list_create'),
-    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyAPIView.as_view(), name='lesson_detail'),
+    path("", include(router.urls)),
+    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson_list_create"),
+    path(
+        "lessons/<int:pk>/",
+        LessonRetrieveUpdateDestroyAPIView.as_view(),
+        name="lesson_detail",
+    ),
 ]
