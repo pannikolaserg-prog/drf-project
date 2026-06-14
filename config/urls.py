e81134aab58f362c8e24383057bc09f99fa1d6e3
Nikolay Panov <panovn.s@yandex.ru>
2026-06-14 23:42:35 +0900

    add final correcting

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.views import PaymentViewSet
+
+router = DefaultRouter()
+router.register(r'payments', PaymentViewSet, basename='payment')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("materials.urls")),
     path('users/', include('users.urls', namespace='users')),
+    path('payments/', include(router.urls)),
 ]
 
 if settings.DEBUG:
