d21944534aedef01a4506e58ac1cbdbb572030f7
Nikolay Panov <panovn.s@yandex.ru>
2026-06-14 16:44:28 +0900

    add lesson in course

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class LessonSerializer(serializers.ModelSerializer):
     count_lesson_with_same_description = serializers.SerializerMethodField()
 
@@ -27,6 +21,25 @@ class LessonSerializer(serializers.ModelSerializer):
                   'created_at',
                   'updated_at',
                   'count_lesson_with_same_description')
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для курсов с выводом уроков"""
+    # Поле для вывода всех уроков курса (связь через related_name='lessons')
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    # Поле для вывода количества уроков
+    lessons_count = serializers.IntegerField(source='lessons.count', read_only=True)
+
+    # Альтернативный вариант через SerializerMethodField
+    # lessons_count = serializers.SerializerMethodField()
+
+    # def get_lessons_count(self, obj):
+    #     return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'description', 'created_at', 'lessons', 'lessons_count']
+        read_only_fields = ['id', 'created_at']
 
 
 class CourseDetailSerializers(serializers.ModelSerializer):
