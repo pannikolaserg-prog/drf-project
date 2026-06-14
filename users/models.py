a250688e5c36ee3ca2c5de96b78e2bfc4a42d398
Nikolay Panov <panovn.s@yandex.ru>
2026-06-14 15:57:13 +0900

    add payment model and fixtures

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
-from platform import machine
-
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -41,3 +41,18 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=[
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод'),
+    ])
+
+    def __str__(self):
+        return f"{self.user} - {self.amount}"
