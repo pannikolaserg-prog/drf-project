5f144be779d881185193ae7537d886ae29f4e570
Nikolay Panov <panovn.s@yandex.ru>
2026-06-14 22:55:13 +0900

    add profile with  payment history

--- users/serializers.py
+++ users/serializers.py
@@ -11,3 +11,11 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = ['id', 'user', 'course', 'lesson', 'amount', 'payment_date', 'payment_method', 'is_successful']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'first_name', 'last_name', 'payments']
