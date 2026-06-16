3119e11570ad230b179ee2ab819c20041e1b7ad7
Nikolay Panov <panovn.s@yandex.ru>
2026-06-16 23:14:48 +0900

    add jwt authorisation

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-app_name = 'users'
+from . import views
+from .apps import UsersConfig
+from .views import UserCreateView
+
+app_name = UsersConfig.name
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='user_list'),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
     path('create/', views.UserCreateView.as_view(), name='user_create'),
     path('<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
