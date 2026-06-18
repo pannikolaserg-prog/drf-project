8e697a4991e30f655c153c4576adc78af2e02d32
Nikolay Panov <panovn.s@yandex.ru>
2026-06-19 00:14:47 +0900

    add moders

--- users/permissions.py
+++ users/permissions.py
@@ -2,7 +2,31 @@ from rest_framework import permissions
 
 
 class IsModer(permissions.BasePermission):
-    message = "Adding customer not allowed"
+    """Разрешение только для модераторов"""
 
     def has_permission(self, request, view):
-        return request.user.groups.filter(name="moders").exists()
+        return request.user.is_authenticated and request.user.groups.filter(name='moderators').exists()
+
+
+class IsNotModer(permissions.BasePermission):
+    """Разрешение для всех, кроме модераторов"""
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and not request.user.groups.filter(name='moderators').exists()
+
+
+class IsOwner(permissions.BasePermission):
+    """Разрешение для владельца объекта"""
+
+    def has_object_permission(self, request, view, obj):
+        # Проверяем, что пользователь является владельцем объекта
+        return obj.owner == request.user
+
+
+class IsOwnerOrModer(permissions.BasePermission):
+    """Разрешение для владельца объекта или модератора"""
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name='moderators').exists():
+            return True
+        return obj.owner == request.user
