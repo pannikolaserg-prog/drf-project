8c7d9f4896a4794274795771f2af70f365207c9c
Nikolay Panov <panovn.s@yandex.ru>
2026-06-20 22:06:22 +0900

    add pagination

--- users/models.py
+++ users/models.py
@@ -97,6 +97,7 @@ class Subscription(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = ('user', 'course')  # Чтобы не было дублей
+        unique_together = ('user', 'course')
