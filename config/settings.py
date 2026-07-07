58dc80a85c0a541d26ba1131d94fe46a172d1762
Nikolay Panov <panovn.s@yandex.ru>
2026-07-07 23:26:44 +0900

    correcting timezone

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,8 @@ CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:63
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Europe/Moscow'
-CELERY_ENABLE_UTC = False
+CELERY_TIMEZONE = 'UTC'
+# CELERY_ENABLE_UTC = False
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 INSTALLED_APPS = [
